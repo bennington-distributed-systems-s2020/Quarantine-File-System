bdb1f6e87bb82d64b972bfa96faeb394fe20bc6b
zalkar-z <zak@packet.com>
2020-05-14 22:08:18 -0400

    [master-recovery] initialized recovery scripts and files

---
+++ master/recover.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+	recover.py - Recovered the master on startup using checkpoints and logs
+	Author: Zalkar Ziiaidin (zalkar@bennington.edu)
+	Date: 5/13/2020
+"""
+
+# takes checkpoint (master.json)
+# plays logs.json on top of it
+# Done! This is our new state
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
