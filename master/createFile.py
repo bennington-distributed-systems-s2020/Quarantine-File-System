0c970a7ef5f132dd407ce4b5decc864daa3687e1
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-05-21 16:42:23 -0400

    added metadata.py unit testing and use description of this module. Incorporated metadata.py, need remove_directory from meta, then unit testing and debug from there. q: who updates the chunk size?

--- master/createFile.py
+++ master/createFile.py
@@ -2,74 +2,151 @@
 file_name: createFile.py
 
 functionality: Used for creating a new file for a client.
-1. it assignes the new file
+1. create_new_directory(directory_path)
+here put absolute directory path such as "/school/work/"
+You can only create one directory at an existing directory at a time
 
-Date: May 17th, 2020
+2. create_file(file_path, file_size)
+file path eg: "/school/work/fun.txt"
+file size: number of bytes of the file you wanna create and append data to.
+
+3. create_new_chunk(file_path)
+file path eg: "/school/work/fun.txt"
+--it creates a new chunk for the file
+
+4. remove_file(file_path, chunk_index)
+file_path: could also be a file path "/school/work/fun.txt"
+chunk_index: optional
+---this function can remove a file or a certain chunk that holds the certain chunk index
+---if chunk index is not given, this function will the whole file
+---otherwise it only removes the chunk has the given index.
+
+
+5. remove_directory(directory_path) ---need to be built
+directory_path eg: "/school/work/"
+this function will remove the directory "work" and everything in it
+if it's valid.
+
+Date: May 21th, 2020
 Author: Zhihong Li
 """
 
 import os
 import json
-from store import store
+from random import randint
+from store import metadata
 
-chunk_handle_counter_json = "chunkHandlerCounter.json"
-logfile_path = "logs.json"
-checkpoint_path = "checkpoint.json"
+#############----Config---#################
+number_of_replicas_json = "numberOfReplicasConfig.json"
 
-metadata_handler = store.MetadataStorage(logfile_path, checkpoint_path)
+# get chunk replicas
+with open(number_of_replicas_json) as f:
+    number_of_replicas = json.load(f)["numberOfReplicas"]
 
-def create_new_file(file_name, directory_path, file_size, meatadata_handler):
-    if file_name is None or type(file_name) != str:
-        return False # could also pass error message with a tuple: (False, "file_name is empty")
-    
-    # verify the path
-    if directory_path is None or type(file_name) != str or meatadata_handler.verify_path(directory_path):
-        return False
-    
-    # verify filesize
-    if file_size is None or (file_size < 0) or type(file_size) != int:
-        return False
-    global chunk_handle_counter_json
-    number_of_chunks_needed = get_chunk_size_needed(file_size)
-    chunk_handle_list = get_chunk_handle(number_of_chunks_needed)
+# get metadata_handler
+metadata_handler = metadata.MetadataStorage.retrieveStorage()
+####################################
 
-    # create new file with number of chunks in the metadata
 
-def create_new_directory(directoryName, directory_path):
+
+def verify_file_parent_directory_path(file_path):
     global metadata_handler
     
-    # make sure the path is valid, traverse the path to the valid directory
+    if type(file_path) != str or len(file_path) < 0:
+        return False # invalid file_path
+    # get file parent direcoty path
+    parent_directory_path = get_file_parent_directory_path(file_path)
 
-    # if valid, add the new directory in the metadata
+#################### need to check return value of the verify function of metadata_handler with FIVE#####################
+   # check parent_directory_path directory exist, if not return False 
+    if metadata_handler.verify_path(parent_directory_path) == True:
+        return True
+    else:
+        return False
+
+
+def get_file_parent_directory_path(file_path):
+    parent_directory_path = file_path.split('/')[:-1]
+    parent_directory_path = '/'.join(parent_directory_path) + '/'
+    return parent_directory_path
+
+
+def create_new_file(file_path, file_size):
+    global metadata_handler
+    global number_of_replicas
+    # verify filesize
+    if (file_size is None) or (type(file_size) != int) or (file_size < 0) :
+        return False
+    if verify_file_parent_directory_path(file_path) is False:
+        return False
+    number_of_chunks_needed = get_number_of_chunk_needed(file_size)
+
+    # create new file with number of chunks needed in the metadata
+    for _ in range(1, number_of_chunks_needed):
+        # get random live server list according to number of replicas set in config
+        live_servers_list = metadata_handler.locate()
+        random_server_list = get_servers_list_that_stores_new_file(live_servers_list, number_of_replicas)
+        
+        # get chunk handle
+        chunk_handle = metadata_handler.get_chunk_handle()
+        # assign new chunks to different servers all the time. distribute chunks well
+        metadata_handler.create_chunk(file_path, chunk_handle, random_server_list) #######verify if this function takes a list of chunkserver?
+    return True # success
+
+def create_new_directory(directory_path):
+    global metadata_handler
+
+    # get parent directory of the directoy path we creating.
+    parent_directory_path = '/'.join(directory_path.split('/')[:-2]) + '/'
+
+    # verify parent directory
+    if verify_file_parent_directory_path(parent_directory_path) == False:
+        return False ################## ask five about what the function returns.
     
-    # log the operation
+    # if valid, add the new directory in the metadata
+    metadata_handler.create_path(directory_path)
+    return True # success
+
+def create_new_chunk(file_path):
+    global metadata_handler
+    global number_of_replicas
+
+    if metadata_handler.verify_path(file_path) is False: ########################## return value need to ask five
+        return False
+    
+    # get random chunkservers list to store the chunk
+    live_chunkservers_list = metadata_handler.locate()
+    new_chunk_hundle = metadata_handler.get_chunk_handle() ################## problem line no return value
+    random_chunk_server_list = get_servers_list_that_stores_new_file(live_chunkservers_list, number_of_replicas)
+
+    # create new chunk for chunkservers on the random list
+    metadata_handler.create_chunk(file_path, new_chunk_hundle, random_chunk_server_list)  ######### ask takes a list of servers?
+    return True # success
 
 def remove_file(file_path):
     global metadata_handler
-    # if fila_path valid:
-        # remove the file
-    # else:
-        # return error code // file does not exist
-    # end
+    if metadata_handler.verify_path(file_path) is not True:
+        return False # invalid path
+    else:
+        metadata_handler.remove(file_path) # remove the file
+        return True
 
-# this is the function that you use to get chunkHandle according to number of chunks needed
-def get_chunk_handle(number_of_chunks_needed):
-    global metadata_handler    
-    handleList = create_chunk_handle(number_of_chunks_needed)
-    return handleList    
-
-def create_chunk_handle(number_of_chunks_needed):
+def remove_directory(directory_path):
     global metadata_handler
-    global chunk_handle_counter_json
-    handleList = []
-    for i in range(number_of_chunks_needed):
-        newHandle = metadata_handler.get_chunk_handle()
-        handleList.append(newHandle)
-    return handleList
 
+    # check if the directory is valid
+    if metadata_handler.verify_path(directory_path) == False:
+        return False # invalid directory
+    
+    # if valid, remove it
+    # ###############-------function is not created yet------#############################
+    metadata_handler.remove_directory(directory_path)
+    return True
 
 # file size is in byte
-def get_chunk_size_needed(file_size):
+def get_number_of_chunk_needed(file_size):
+    if type(file_size) is not int or file_size < 0:
+        return False # wrong input return False
     one_mb = 1000000 # 1mb = 1000000 bytes
     chunk_size_offset = 16 
     one_chunk_size_in_byte = one_mb * 64 - chunk_size_offset
@@ -81,29 +158,69 @@ def get_chunk_size_needed(file_size):
         number_of_chunks_needed = int(file_size / one_chunk_size_in_byte) + 1
         return number_of_chunks_needed
 
+
+def get_servers_list_that_stores_new_file(live_servers_list, number_of_replicas):
+    live_servers_num = len(live_servers_list)
+    random_server_list = []
+    if live_servers_num <= 0:
+        return False
+    for _ in range(number_of_replicas):
+        randomNum = randint(0,live_servers_num-1)
+        random_server_list.append(live_servers_list[randomNum])
+    return random_server_list
+
+
+
+
 if __name__ == "__main__":
-    # testing
-    r = get_chunk_handle(20)
-    print(r)
-
+    # test get chunk size needed
+    assert get_number_of_chunk_needed(-1) == False, "failed test"
+    assert get_number_of_chunk_needed("19") == False, "failed test"
+    assert get_number_of_chunk_needed(9999999999) == 157, "failed test"
     
-    """
-    getchunk- returns a list [chunkhandle, size, [replicas]]
+    # test get chunk handle
+    assert type(metadata_handler.get_chunk_handle()) == str, "Failed to get chunk handle"
 
-    Inputs: 
-    file name
-    folder Path (namespace)
-    file_size(unit MB)
+    # test get live_server list from metadata object
+    assert type(metadata_handler.locate()) == list, "failed to get live server list"
 
-    What happens in the function:
-    Get number of chunks needed for the file
-    create file metadata (namespace, chunks with handles)
-    Assign three chunkservers to own the file. (chunkservers that holds the file namespace and chunks)
-    Create metadata for the new file (includes file namespace:chunks(with index) and handles, chunkservers
-    that holds it)
-    Append new metadata and namespace to the namespace-metadata map file
-    Assign a primary for all the chunks and return it and all other replicas locations. (maybe chunkservers 
-    that holds the chunks) because the client will ask the chunkserver for access and write or read. If 
-    chunkserver is not primary, they will ask us for it. (might be some special cases here)
+    # test creating new path
+    assert metadata_handler.create_path("/school/") == """some success return value here""", "failed to create directory"
+    assert metadata_handler.create_path("/school/cs/") == """some success return value here""", "failed to create directory"
+    assert metadata_handler.create_path("/school/music/") == """some success return value here""", "failed to create directory"
 
-    """
+    # test verify existing directory path
+    assert metadata_handler.verify_path("/school/") == """some success return value here""", "failed to verify directory"
+    assert metadata_handler.verify_path("/school/cs/") == """some success return value here""", "failed to verify directory"
+    assert metadata_handler.verify_path("/school/music/") == """some success return value here""", "failed to verify directory"
+
+    # test the method of getting directory's parent directory path
+    parent_directory_path = '/'.join("/school/cs/".split('/')[:-2]) + '/'
+    assert get_file_parent_directory_path("/school/cs/") == "/school/", "failed to get directory's parent directory path"
+
+    # test creating a file
+    assert create_new_file("school/cs/fun.txt", 10000000) == True, "failed to create file"
+    assert create_new_file("school/cs/hello.txt", 50) == True, "failed to create file"
+    assert metadata_handler.verify_path("school/cs/fun.txt") == True, "failed to remove a file"
+    assert metadata_handler.verify_path("school/cs/hello.txt") == True, "failed to remove a file"
+
+    # test create a new chunk for an existing file
+    live_chunk_server_list = ["server1", "server2", "server3"]
+    assert metadata_handler.create_chunk("school/cs/fun.txt","2299aac92", live_chunk_server_list) == True, "failed to create new chunk for existing file"
+
+    # test getting a file's directory path
+    assert get_file_parent_directory_path("school/cs/fun.txt") == "school/cs/", "failed to get file parent directory path"
+
+    # test remove a directory
+    assert metadata_handler.verify_path("school/music/") == True, "failed to get file parent directory path"
+    assert metadata_handler.remove("school/music/") == True, "failed to get file parent directory path"
+    assert metadata_handler.verify_path("school/music/") == False, "failed to get file parent directory path"
+
+    # test remove a file
+    assert metadata_handler.remove("school/cs/fun.txt") == True, "failed to remove a file"
+    assert metadata_handler.verify_path("school/cs/fun.txt") == False, "failed to remove a file"
+
+    # test remove a directory that has file in it
+    assert metadata_handler.remove("school/cs/") == True, "failed to remove a directory that has file in it"
+    assert metadata_handler.verify_path("school/cs/fun.txt") == False, "failed to remove a directory that has file in it"
+    assert metadata_handler.remove("school/cs/") == False, "failed to remove a directory that has file in it"
