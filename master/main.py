0d334fdf5bb72540b0b77f5642755f7dad5df6aa
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-05-22 01:33:21 -0400

    updated fetch function in main, renamed createFile to file_management.py

--- master/main.py
+++ master/main.py
@@ -4,8 +4,8 @@
     Date: 5/12/2020
 """
 
-from flask import Flask
-from metadata import Metadata
+from flask import Flask, jsonify
+from file_management import *
 
 app = Flask(__name__)
 
@@ -14,17 +14,25 @@ def example():
     return "Flask!"
 
 
-# Question to Client team: is chunk_index a string or intiger?
+# Question to Client team: is chunk_index a string or intiger? 
 @app.route('/fetch/<string:file_name>/<int:chunk_index>', methods=['GET'])
-def fetch():
+def fetch(file_name, chunk_index):
+    if metadata_handler.verify_path(file_name) == False:
+        return "invalid file path"
+    
+    # since this function takes a list of index, so I used [chunk_index] to make it a list
+    json_response = {}
+    chunk_info = get_file_chunk_handles(file_name, [chunk_index])[0]
+    json_response["chunk_handle"] = chunk_info[0]   # extract chunk handle
+    json_response["chunk_servers"] = chunk_info[2]  # extract replica locations
+    return jsonify(json_response)
+    
     """
     Caller: Client
     :param file_name: Name of the file being requested  
     :param chunk_index: A specific part of the file being requested 
     :return: chunk handle and chunk locations as JSON
     """
-    return "..."
-
 
 @app.route('/heartbeat', methods=['POST'])
 def heartbeat():
