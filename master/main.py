8ada73e6f4c50d66989c50342d070056e47342d0
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-05-24 11:51:45 -0400

    finished heartbeat api

--- master/main.py
+++ master/main.py
@@ -14,9 +14,16 @@ def example():
     return "Flask!"
 
 
-# Question to Client team: is chunk_index a string or intiger? 
+# need to reform this function so that chunkserver can call and create new chunks or files
+# need a new parameter called command, start_byte
 @app.route('/fetch/<string:file_name>/<int:chunk_index>', methods=['GET'])
 def fetch(file_name, chunk_index):
+    """
+    Caller: Client
+    :param file_name: Name of the file being requested  
+    :param chunk_index: A specific part of the file being requested 
+    :return: chunk handle and chunk locations as JSON
+    """
     if metadata_handler.verify_path(file_name) == False:
         error = {"error": "invalid file path"}
         return jsonify(error)
@@ -25,24 +32,26 @@ def fetch(file_name, chunk_index):
     json_response = {}
     chunk_info = get_file_chunk_handles(file_name, [chunk_index])[0]
     json_response["chunk_handle"] = chunk_info[0]   # extract chunk handle
-    json_response["chunk_servers"] = chunk_info[2]  # extract replica locations
+    
+    # need to add chunk_primary into this return data. if it's r, w, or a operation.
+
+    json_response["replica"] = chunk_info[2]  # extract replica locations
     return jsonify(json_response)
     
-    """
-    Caller: Client
-    :param file_name: Name of the file being requested  
-    :param chunk_index: A specific part of the file being requested 
-    :return: chunk handle and chunk locations as JSON
-    """
 
-@app.route('/heartbeat', methods=['POST'])
-def heartbeat():
+@app.route('/heartbeat/<string: chunk_server_ip>/<bool: chunk_server_state>', methods=['POST'])
+def heartbeat(chunk_server_ip,chunk_server_state):
     """
     Caller: Chunkserver
-    :param state: Boolean value to check if a server is alive
-    :return: Always "OK" with a status code of 200.
+    :param chunk_server_ip: chunk_server_ip/dns
+    :param chunk_server_state: bool, True(chunk server is available), False(unavailable) 
+    :return: status code 200 means"OK"; code 500 means "error" 
     """
-    return "OK"
+    try:
+        metadata_handler.toggle(chunk_server_ip, chunk_server_state)
+    except:
+        return 500
+    return 200
 
 
 @app.route('/lease-request/<string:chunk_handle>', methods=['GET'])
