c7af72cec5aba3152e76cae0ace559c807c1de62
Rose/Roxy <47798467+lonelyVoxel@users.noreply.github.com>
2020-05-26 22:12:39 -0400

    Create and Read implementation

--- client/Frontend.py
+++ client/Frontend.py
@@ -4,17 +4,14 @@
 # Author: Rose/Roxy Lalonde {roseernst@bennington.edu}
 # edited by ell 5/20/20
 # Date created: 5/18/2020
-# Some of the parameters in the functions might be part of the metadata.
-# Perhaps the return from the metadata function should be to put the relevant data in a list,
-# so that other functions can easily pull from it
+# Edited again by Roxy 5/26/20
 #
 
-from flask import Flask, json, jsonify, abort
-from pip._vendor import requests
-
+from flask import Flask, json, jsonify, abort, request
+import requests
 import client_append
-
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -22,13 +19,15 @@ app = Flask(__name__)
 with open("client.json") as client_json:
     client_config = json.load(client_json)
 
+
 @app.route("/")
 def example():
     return "client flask API"
 
-#fetch is only a master side command
-#@app.route('/fetch/<string:file_name>/<int:chunk_index>')
-#def fetch(file_name, chunk_index):
+
+# Will replace this with the fetch code called inside all of the other functions
+# @app.route('/fetch/<string:file_name>/<int:chunk_index>')
+# def fetch(file_name, chunk_index):
 #    """
 #    Makes a request to the master for the metadata of a file. This will provide important
 #    information for the rest of the commands in this file.
@@ -38,26 +37,28 @@ def example():
 #    """
 #    metadata = 0  # Will have some kind of call to the master to get the metadata list
 #    #Q: Call fetch on master for this
-#    return metadata
+#    if metadata = 0
+#        programmer.cry(tears_of_profound_sadness)
+#    else
+#        return metadata
 
 @app.route('/create/<string:file_name>')
 def create(file_name):
-    #Call fetch on Master to create the file
-    fetch_r = requests.get("http://{0}:{1}/fetch".format(client_config["master"][0],
-                                                         client_config["master"][1]),
-                                                         json={json.dumps({"filename": file_name, "command": "c"})})
-    fetch_status = fetch_r.status_code()
-    if fetch_status == 500:
+    # Call fetch on Master to create the file
+    fetch_r = requests.get("http://{0}:{1}/fetch".format(client_config["master"][0], client_config["master"][1]),
+                           json={json.dumps({"filename": file_name, "command": "c"})})
+    if fetch_r.status_code == 500:
         app.logger.critical("Exception on master when creating {0}".format(file_name))
         abort(500)
-    elif fetch_status != 200:
+    elif fetch_r.status_code != 200:
         app.logger.warning("Unknown error on Master when trying to create {0}".format(file_name))
         abort(500)
     else:
-        return 0 #success
+        return 0  # success
 
-#Q: rewrote the spec for read. Note that the client does not have access to the metadata
-#Q: It'd have to call fetch to the Master in order to get the chunkhandle to read from
+
+# Q: rewrote the spec for read. Note that the client does not have access to the metadata
+# Q: It'd have to call fetch to the Master in order to get the chunkhandle to read from
 @app.route('/read/<string:file_name>,<int:start_byte>,<int:byte_range>', method='GET')
 def read(file_name, start_byte, byte_range):
     """
@@ -67,34 +68,18 @@ def read(file_name, start_byte, byte_range):
     :param byte_range: How much to read
     :return: Text obtained from server
     """
-    # Initialize some variables for error handling and return stuff
-    output = 0
-
-    # Wait for two minutes for the chunkserver to respond.
-    while output == 0:
-        # json = {chunk_handle: self.chunk_handle} was a parameter in Quang's example, but what is self?
-        server_request = requests.get("http://{0}/read-request".format("URL"), timeout=90)
-
-        # What function should I call to tell the server "hey give me stuff?"
-        # output = {some call, does it need to have the parameters in ir or is that handled in the app route?}
-
-
-        """
-        for i in range(0, 12):
-            if output != 0:
-                break
-            time.sleep(10)
-            i = i + 1
-        break
-        """
-
-    # Output things! As long as output has changed, the function should give the package from the chunkserver
-    if output != 0:
-        return output
+    # Fetch things from the server.
+    fetch_r = requests.get("http://{0}:{1}/fetch".format(client_config["master"][0], client_config["master"][1]),
+                           json={json.dumps({"filename": file_name, "command": "r", "startbyte": start_byte,
+                                             "byterange": byte_range})})
+    if fetch_r.status_code == 500:
+        app.logger.critical("Exception on master when reading {0}".format(file_name))
+        abort(500)
+    elif fetch_r.status_code != 200:
+        app.logger.warning("Unknown error on Master when trying to read {0}".format(file_name))
+        abort(500)
     else:
-        print("An error has occurred.")
-
-    # Pass this all to the server, get back a status, and print a message to the user
+        return fetch_r  # success
 
 
 @app.route('/write/<string:file_name>,<string:content>')
@@ -109,8 +94,9 @@ def write(file_name, content):
     abort(405)
     return ""
 
-#modifying append and rewrite it to take in a json instead
-#since it might not be a good idea to fit 16mb of content in an url
+
+# modifying append and rewrite it to take in a json instead
+# since it might not be a good idea to fit 16mb of content in an url
 @app.route('/append/', strict_slashes=False, methods=['GET', 'POST'])
 def append(file_name, content):
     """
@@ -120,46 +106,38 @@ def append(file_name, content):
     :return: Success confirmation or error message {documentation says boolean though?}
     """
     # Pass this all to the server, get back a status, and print a message to the user
-    #parsing json
+    # parsing json
     request_json = request.get_json()
     file_name = request_json["file_name"]
     content = request_json["content"]
 
-    #Call fetch on Master to get the primary and the replicas
+    # Call fetch on Master to get the primary and the replicas
     fetch_r = requests.get("http://{0}:{1}/fetch".format(client_config["master"][0],
                                                          client_config["master"][1]),
-                                                         json={json.dumps({"filename": file_name, "command": "a"})})
-    #see API doc for return format
-    append_chunk = fetch_r.json()[0] #if fetch was done correctly this should just have 1 chunk in the list
+                           json={json.dumps({"filename": file_name, "command": "a"})})
+    # see API doc for return format
+    append_chunk = fetch_r.json()[0]  # if fetch was done correctly this should just have 1 chunk in the list
 
-    #call append
+    # call append
     return_code = client_append.append(append_chunk, content)
-    #if we have to append again on a new chunk
+    # if we have to append again on a new chunk
     if return_code == 301:
         fetch_new_r = requests.get("http://{0}:{1}/fetch".format(client_config["master"][0],
                                                                  client_config["master"][1]),
-                                                                 json={json.dumps({"filename": file_name, "command": "ac"})})
+                                   json={json.dumps({"filename": file_name, "command": "ac"})})
         new_append_chunk = fetch_new_r.json()[0]
-        #call the append function again on the new chunk
+        # call the append function again on the new chunk
         return_code = client_append.append(new_append_chunk, content)
 
-    #on a new chunk we could not get 301
+    # on a new chunk we could not get 301
     if return_code == 500:
         app.logger.critical("Exception on chunkserver {0}".format(append_chunk["primary"]))
         abort(500)
     elif return_code == 400:
-        #what could cause this is unknown. We could attempt to run the func again ehre
-        #though for now ill just log and abort
-        app.logger.warning("Chunkhandle not found or buffer not found on chunkserver {0} or one of its replicas".format(append_chunk["primary"]))
+        # what could cause this is unknown. We could attempt to run the func again ehre
+        # though for now ill just log and abort
+        app.logger.warning("Chunkhandle not found or buffer not found on chunkserver {0} or one of its replicas".format(
+            append_chunk["primary"]))
         abort(400)
     else:
-        return 0 #success
-
-#this is actually unnecessary so ill comment it out
-#@app.route('/append_request/<string:chunk_handle>')
-#def append_request(chunk_handle):
-#    """
-#    Asks the chunk server for permission to append to a file
-#    :param chunk_handle: File ID
-#    :return: Success confirmation or error message
-#    """
+        return 0  # success
